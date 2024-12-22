a3f2a066c4d10ebd8ee7159dcd0734b2f0411870
andirea19 <ungerer.andrea@gmail.com>
2024-12-22 12:46:23 +0100

    Überlegungen Menp, weil Konsolen app

--- Program.cs
+++ Program.cs
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace termini
 {
-    internal class Program
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    // Listen für Aufgaben und Geburtstage
+    static List<Task> toDoListe = new List<Task>();
+    static List<Birthday> geburtstage = new List<Birthday>();
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        while (true)
         {
+            Console.Clear();
+            Console.WriteLine("Willkommen zur To-Do- und Kalenderanwendung!");
+            // Aufgaben an Geburtstage koppeln? 
+            Console.WriteLine("1. Aufgabe hinzufügen");
+            Console.WriteLine("2. Aufgaben anzeigen");
+            Console.WriteLine("3. Geburtstag anlegen");
+            // Alternativ: Ende? 
+            Console.WriteLine("4. Geburtstage anzeigen");
+            Console.WriteLine("5. Programm beenden");
+            Console.Write("Wähle eine Option: ");
+
+            string auswahl = Console.ReadLine();
+            switch (auswahl)
+            {
+                case "1":
+                    AufgabeHinzufügen();
+                    break;
+                case "2":
+                    AufgabenAnzeigen();
+                    break;
+                case "3":
+                    GeburtstagSpeichern();
+                    break;
+                case "4":
+                    GeburtstageAnzeigen();
+                    break;
+                case "5":
+                    return;
+                default:
+                    Console.WriteLine("Ungültige Auswahl. Drücke eine beliebige Taste...");
+                    Console.ReadKey();
+                    break;
+            }
         }
     }
+
+
 }
